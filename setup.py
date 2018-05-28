e8917bee3b3d50d72e7b5580ed1f1564bf7b676e
bsnacks000 <bsnacks000@gmail.com>
2018-05-28 16:22:17 -0400

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='nose.collector',
     tests_require=test_requirements,
     url='https://github.com/bsnacks000/bds',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )

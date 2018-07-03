5c7711ac49d19d35f3369fbd897baf55d292914d
bsnacks000 <bsnacks000@gmail.com>
2018-07-03 10:32:00 -0400

    removed calc_factory from binx.__init__

--- binx/__init__.py
+++ binx/__init__.py
@@ -7,6 +7,5 @@ __email__ = 'bsnacks000@gmail.com'
 __version__ = '0.2.0'
 
 from .collection import BaseCollection, BaseSerializer, InternalObject
-from .calc_factory import BaseFactory, AbstractCalc, AbstractCalcResult
 
-__all__ = ['BaseFactory', 'AbstractCalc', 'AbstractCalcResult', 'BaseCollection', 'InternalObject', 'BaseSerializer']
+__all__ = [ 'BaseCollection', 'InternalObject', 'BaseSerializer']

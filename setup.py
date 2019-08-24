a5860358c19fa8240921d7731e19050e4ddca95c
bsnacks000 <bsnacks000@gmail.com>
2019-08-23 20:37:22 -0400

    pin to marshmallow-2.2.20 until I have time to update to 3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['pandas', 'marshmallow' ]
+requirements = ['pandas', 'marshmallow==2.20.2' ]
 
 setup_requirements = [ ]
 

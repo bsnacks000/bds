72169d8dddc7bf411815e649eb632210aabf1efe
bsnacks000 <bsnacks000@gmail.com>
2019-08-25 10:27:04 -0400

    fixed dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['pandas', 'marshmallow==2.20.2' ]
+requirements = ['pandas', 'marshmallow>=3.0' ]
 
 setup_requirements = [ ]
 

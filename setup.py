cb377c2639ad188cfe024ad16f29d0180065e7f1
bsnacks000 <bsnacks000@gmail.com>
2019-09-23 12:54:39 -0400

    pinning to marhsmallow==3.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['pandas', 'marshmallow>=3.0' ]
+requirements = ['pandas', 'marshmallow==3.1.1' ]
 
 setup_requirements = [ ]
 

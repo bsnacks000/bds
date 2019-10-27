04c944cfcf453c05bbdb73ebfaef01fdb287b771
bsnacks000 <bsnacks000@gmail.com>
2019-10-27 12:19:54 -0400

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='nose.collector',
     tests_require=test_requirements,
     url='https://github.com/bsnacks000/binx',
-    version='0.4.1',
+    version='0.4.2',
     zip_safe=False,
 )
